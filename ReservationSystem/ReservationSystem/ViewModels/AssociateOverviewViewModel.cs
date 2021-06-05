a0f3c6448973ff9bb5d7a496d8665ec6bde28141
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-06-05 17:49:32 +0200

    feat: added a few functions

---
+++ ReservationSystem/ReservationSystem/ViewModels/AssociateOverviewViewModel.cs
@@ -0,0 +1,68 @@
+﻿using ReservationSystem.Commands;
+using ReservationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ReservationSystem.ViewModels
+{
+    public class AssociateOverviewViewModel : BaseViewModel
+    {
+        public List<Associate> Associates { get; set; }
+
+        public Associate SelectedAssociate { get; set; }
+        public ICommand UpdateViewCommand
+        {
+            get; set;
+        }
+        public ICommand AddNewOfferForAssociateCommand
+        {
+            get; set;
+        }
+        public ICommand AddNewAssociateCommand
+        {
+            get; set;
+        }
+        public AssociateOverviewViewModel(ICommand updateViewCommand)
+        {
+            UpdateViewCommand = updateViewCommand;
+            SelectedAssociate = null;
+            Associates = getAssociates();
+            AddNewOfferForAssociateCommand = new DelegateCommand(AddNewOfferForAssociate);
+            AddNewAssociateCommand = new DelegateCommand(AddNewAssociate);
+        }
+
+        public List<Associate> getAssociates()
+        {
+            List<Associate> associates = new List<Associate>();
+            using(var db = new ProjectDatabase())
+            {
+                associates = db.Associates.ToList();
+            }
+            return associates;
+        }
+
+        public void AddNewOfferForAssociate()
+        {
+            if (SelectedAssociate != null)
+            {
+                Console.WriteLine(SelectedAssociate.Address);
+                UpdateViewCommand.Execute(new CreateNewOfferViewModel(UpdateViewCommand, SelectedAssociate));
+            }
+            else
+            {
+                MessageBox.Show("Niste izabrali saradnika za kog se kreira ponuda!");
+            }
+        }
+        public void AddNewAssociate()
+        {
+            
+                UpdateViewCommand.Execute(new CreateNewAssociateViewModel(UpdateViewCommand));
+            
+        }
+    }
+}
--- ReservationSystem/ReservationSystem/Views/Organizer/OrganizerAssociateOverview.xaml.cs
+++ ReservationSystem/ReservationSystem/Views/Organizer/OrganizerAssociateOverview.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ReservationSystem.Views
+namespace ReservationSystem.Views.Organizer
 {
     /// <summary>
-    /// Interaction logic for PregledSaradnika.xaml
+    /// Interaction logic for OrganizerAssociateOverview.xaml
     /// </summary>
-    public partial class PregledSaradnika : Window
+    public partial class OrganizerAssociateOverview : UserControl
     {
-        public PregledSaradnika()
+        public OrganizerAssociateOverview()
         {
             InitializeComponent();
         }
